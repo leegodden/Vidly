9c28e353fad16bce141bc4ba9bc5ee727acecc50
rifkijones <rifki.jones@yahoo.co.uk>
2020-01-11 13:18:11 +0000

    Added Customer and Random, MovieViewModel classes & Partial view for Nav bar

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -4,18 +4,38 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // Temp hardcode in Random action
+        // temp hardcode in Random action
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek" };
-            return View(movie);
-            
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1"},
+                new Customer { Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+          
+            return View(viewModel);
         }
+
+        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1, 12)}")]
+        public ActionResult ByReleaseYear(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        
 
         public ActionResult Edit(int id)
         {
